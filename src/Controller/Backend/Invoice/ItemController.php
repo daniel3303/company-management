159e3d4ac924a43851ad67d18d6f57cb55296090
Daniel Oliveira <daniel-oliveira-11@hotmail.com>
2019-06-12 22:34:48 +0100

    Added filters to product sells

--- src/Controller/Backend/Invoice/ItemController.php
+++ src/Controller/Backend/Invoice/ItemController.php
@@ -6,7 +6,10 @@ use App\Controller\Backend\BaseController;
 use App\Entity\Invoice\Invoice;
 use App\Entity\Invoice\Item;
 use App\Form\Invoice\InvoiceType;
+use App\Repository\Company\CompanyRepository;
 use App\Repository\Invoice\InvoiceRepository;
+use App\Repository\Invoice\ItemRepository;
+use App\Repository\Product\ProductRepository;
 use Knp\Component\Pager\Pagination\PaginationInterface;
 use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,6 +21,20 @@ use Symfony\Component\Routing\Annotation\Route;
  * @Route("/backend/invoice/item")
  */
 class ItemController extends BaseController {
+    /**
+     * @Route("/", name="backend_invoice_item_index", methods={"GET"})
+     */
+    public function index(ItemRepository $itemRepository, ProductRepository $productRepository, CompanyRepository $companyRepository, Request $request): Response {
+        $items = $this->paginateWithSorting($itemRepository, $request);
+        /** @var $items Item[]|Pagerfanta */
+
+
+        return $this->render('backend/invoice/item/index.html.twig', [
+            'items' => $items,
+            'products' => $productRepository->findAll(),
+            'companies' => $companyRepository->findAll(),
+        ]);
+    }
 
     /**
      * @Route("/{id}", name="backend_invoice_item_delete", methods={"DELETE"})
